846e1089423d6cf8169759f4956eabe09f027885
MdShahinDev <hello.shahinofficial@gmail.com>
2025-01-24 23:00:34 +0600

    Subscribe Template

--- src/Components/EmailSubscription.jsx
+++ src/Components/EmailSubscription.jsx
@@ -7,9 +7,9 @@ const EmailSubscription = () => {
       <div className="emailsubscription my-8 min-h-64 lg:min-h-96 bg-emailsubscription bg-cover bg-no-repeat bg-fixed flex items-center justify-center">
         <div className="promotext text-center">
             <h2 className='text-base font-semibold text-white md:text-xl lg:text-4xl'>Get Latest Update and Discount offer Mail In your Inbox</h2>
-            <div className="form my-4 flex relative items-center gap-1">
-            <input className='w-full p-2 bg-gray-400 border-none rounded-md focus:ring-0 focus:outline-none' type="text" placeholder='youremail@gmail.com' />
-            <button className='text-black text-2xl absolute right-0 pr-3'> <FaLocationArrow/> </button>
+            <div className="form my-4 flex items-center">
+            <input className='w-full py-2 px-4 bg-white border-none  focus:ring-0 focus:outline-none' type="text" placeholder='youremail@gmail.com' />
+            <button className=' uppercase text-base border-black py-2 px-4 bg-black text-white font-semibold'> Subscribe </button>
             </div>
         </div>
 
